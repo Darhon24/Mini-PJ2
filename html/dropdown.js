ee7581f6c0bad8655f30d908cc210cce20a44921
Darhon24 <darhondasilva@icloud.com>
2022-12-08 15:45:43 -0500

    dropdown rough draft

---
+++ html/dropdown.js
@@ -0,0 +1,33 @@
+console.log('test')
+let selectname = document.querySelector('select')
+function fetchData() {
+    fetch('http://localhost:8083/api/users')
+        .then(response => response.json())
+        .then(data => {
+            for (let i = 0; i < data.length; i++) {
+                console.log(data)
+
+                let opt = document.createElement("option");
+                opt.innerHTML = `${data[i].name}`;
+                opt.value = data[i].id;
+                console.log(opt)
+                selectname.appendChild(opt)
+            }
+        })
+}
+    fetchData()
+// add eventlistener to selectName
+selectname.addEventListener('change',getTask)
+// define a function that fetch data from endpoint
+
+function getTask() {
+   fetch('http://localhost:8083/api/todos')
+        .then(response => response.json())
+        .then(json => {
+            console.log(json)
+
+
+        } )
+
+
+}
